4913c6f3d1250e2ff69bbab8cef7a012b3c52efe
daverolo <107847185+daverolo@users.noreply.github.com>
2023-05-05 22:31:44 +0200

    FEAT: add method to request validator states chunked

--- src/services/eth.ts
+++ src/services/eth.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-type EthValidatorStatus =
+export type EthValidatorStatus =
   | "pending_initialized"
   | "pending_queued"
   | "active_ongoing"
@@ -51,14 +51,56 @@ class ETH {
     validatorIds: string[];
     status: EthValidatorStatus[];
   }) {
+    const params: any = {};
+
+    if (validatorIds.length) params.id = validatorIds.join(",");
+
+    if (status.length) params.status = status.join(",");
+
     const res = await this.agent.get<GetStateValidatorsResponse>(`/eth/v1/beacon/states/${stateId}/validators`, {
-      params: {
-        id: validatorIds.join(","),
-        status: status.join(","),
-      },
+      params: params,
     });
 
     return res.data;
+  }
+
+  async getStateValidatorsChunked({
+    stateId,
+    validatorIds,
+    status,
+    chunkSize,
+  }: {
+    stateId: string;
+    validatorIds: string[];
+    status: EthValidatorStatus[];
+    chunkSize: number;
+  }) {
+    const chunks = [];
+    let keys: string[] = [];
+    for (const key of validatorIds) {
+      keys.push(key);
+      if (keys.length == chunkSize) {
+        chunks.push(keys);
+        keys = [];
+      }
+    }
+    if (keys.length) chunks.push(keys);
+    let resultData: any[] = [];
+    let result: GetStateValidatorsResponse = {
+      execution_optimistic: false,
+      finalized: false,
+      data: [],
+    };
+    for (const chunk of chunks) {
+      result = await this.getStateValidators({
+        stateId: stateId,
+        validatorIds: chunk,
+        status: status,
+      });
+      resultData = resultData.concat(result.data);
+    }
+    result.data = resultData;
+    return result;
   }
 }
 
